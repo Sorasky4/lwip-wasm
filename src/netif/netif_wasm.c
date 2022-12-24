882d3e3735f8e769e44a6b4d1feb0d189dc72c09
sora <kacky1210@gmail.com>
2022-12-24 19:37:24 +0900

    use inet_pton instead of inet_addr

--- src/netif/netif_wasm.c
+++ src/netif/netif_wasm.c
@@ -11,8 +11,12 @@ int netif_wasm_add(const char* ipaddr, const char* netmask)
   if (netif_default != NULL) {
     return -1;
   }
-  in_addr_t addr = inet_addr(ipaddr);
-  in_addr_t mask = inet_addr(netmask);
+  // in_addr_t addr = inet_addr(ipaddr);
+  // in_addr_t mask = inet_addr(netmask);
+  in_addr_t addr;
+  in_addr_t mask;
+  inet_pton(AF_INET, ipaddr, &addr);
+  inet_pton(AF_INET, netmask, &mask);
   netif_wasm.ip_addr.addr = addr;
   netif_wasm.netmask.addr = mask;
   printf("ipaddr=0x%08x\n", addr);
